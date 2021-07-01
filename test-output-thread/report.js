43efc23f8b1da3640cd5a9b05646d0026f4d257d
Asghar <salar2015151@gmail.com>
2021-06-30 23:06:53 -0400

    PDF report and Spark report added

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "c1516d82-12f0-40be-b066-6ef7756ec07c",
-    "feature": "Login page feature",
-    "scenario": "Login with correct credentials",
-    "start": 1625100076451,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1625100085321,
-    "className": "passed"
-  },
-  {
-    "id": "c90e5d5f-a3aa-4628-8a83-1de1c26f8116",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts page title",
-    "start": 1625100034935,
-    "group": 1,
-    "content": "",
-    "tags": "@accounts,",
-    "end": 1625100048561,
-    "className": "passed"
-  },
-  {
-    "id": "7bb76939-5904-4b0f-871e-f197fdbae08b",
-    "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1625100058111,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1625100067656,
-    "className": "passed"
-  },
-  {
-    "id": "0a3c2d1e-7b09-4baa-ba9b-607570215d41",
+    "id": "6d3ee8e2-b89b-43c9-aa7e-dc4dd7749a3b",
     "feature": "Account Page Feature",
     "scenario": "Accounts section count",
-    "start": 1625100048612,
+    "start": 1625108008617,
     "group": 1,
     "content": "",
     "tags": "@accounts,",
-    "end": 1625100058100,
+    "end": 1625108018778,
     "className": "passed"
   },
   {
-    "id": "bf6c2e2c-1415-4b12-827f-bc55282869d2",
+    "id": "3167a497-e3de-466c-b869-47f24c487b0e",
     "feature": "Login page feature",
     "scenario": "Forgot Password link",
-    "start": 1625100067688,
+    "start": 1625108026498,
     "group": 1,
     "content": "",
     "tags": "",
-    "end": 1625100076437,
+    "end": 1625108037130,
+    "className": "passed"
+  },
+  {
+    "id": "d0dcabe5-4268-4311-8cd4-ca692a7aec93",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1625107992656,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1625108008585,
+    "className": "passed"
+  },
+  {
+    "id": "c416d580-d7c0-4fe7-a694-205d986e2627",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1625108037147,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1625108047287,
+    "className": "passed"
+  },
+  {
+    "id": "29e1077d-61ef-4cd6-ae10-c4f21a461654",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1625108018833,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1625108026478,
     "className": "passed"
   }
 ]);
