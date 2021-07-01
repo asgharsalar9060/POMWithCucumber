55316d7addf6b4f28fe24e82a52559346e5efacc
Asghar <salar2015151@gmail.com>
2021-07-01 11:47:11 -0400

    Parallel execution plugin added

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "6d3ee8e2-b89b-43c9-aa7e-dc4dd7749a3b",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts section count",
-    "start": 1625108008617,
-    "group": 1,
-    "content": "",
-    "tags": "@accounts,",
-    "end": 1625108018778,
-    "className": "passed"
-  },
-  {
-    "id": "3167a497-e3de-466c-b869-47f24c487b0e",
-    "feature": "Login page feature",
-    "scenario": "Forgot Password link",
-    "start": 1625108026498,
-    "group": 1,
-    "content": "",
-    "tags": "",
-    "end": 1625108037130,
-    "className": "passed"
-  },
-  {
-    "id": "d0dcabe5-4268-4311-8cd4-ca692a7aec93",
-    "feature": "Account Page Feature",
-    "scenario": "Accounts page title",
-    "start": 1625107992656,
-    "group": 1,
-    "content": "",
-    "tags": "@accounts,",
-    "end": 1625108008585,
-    "className": "passed"
-  },
-  {
-    "id": "c416d580-d7c0-4fe7-a694-205d986e2627",
+    "id": "9eaf64cd-0ba8-46f1-b5a6-d8ddbffe6496",
     "feature": "Login page feature",
     "scenario": "Login with correct credentials",
-    "start": 1625108037147,
+    "start": 1625154100622,
     "group": 1,
     "content": "",
     "tags": "",
-    "end": 1625108047287,
+    "end": 1625154113327,
     "className": "passed"
   },
   {
-    "id": "29e1077d-61ef-4cd6-ae10-c4f21a461654",
+    "id": "1f39a81e-7421-457d-a311-9fa9aa1a1b2e",
     "feature": "Login page feature",
-    "scenario": "Login page title",
-    "start": 1625108018833,
+    "scenario": "Forgot Password link",
+    "start": 1625154083592,
     "group": 1,
     "content": "",
     "tags": "",
-    "end": 1625108026478,
+    "end": 1625154100611,
+    "className": "passed"
+  },
+  {
+    "id": "6728f689-04b3-405e-b2cf-a04946d37c60",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1625154018362,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1625154042543,
+    "className": "failed"
+  },
+  {
+    "id": "9f512786-a8f2-41e1-a21c-c90cd7427b71",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1625154042592,
+    "group": 1,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1625154063788,
+    "className": "failed"
+  },
+  {
+    "id": "46ff0818-86fb-4b78-99ef-4f58391ba97f",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1625154063831,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1625154083557,
     "className": "passed"
   }
 ]);
